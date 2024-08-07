2b9c5ea611d688f8ed2254aeba452d5b277e763e
akhaleet <bojantomovic333@gmail.com>
2024-08-07 04:57:07 +0200

    I modified the code a bit, fixed a lot of bugs, and created my first shape in Tetris (a square) that can be moved by clicking a key on the keyboard and that cannot cross the borders of the table.

--- tetriskriptaIGRA.js
+++ tetriskriptaIGRA.js
@@ -50,9 +50,101 @@ function traziIme(){
     }
 }
 
+let K = [
+    { i: 0, j: 4 },
+    { i: 0, j: 5 },
+    { i: 1, j: 4 },
+    { i: 1, j: 5 }
+];
 
 
+function obojiKoordinate() {
+    let table = document.querySelector('#tetris table');
+    let redovi = table.rows;
 
+    for (let i = 0; i < redovi.length; i++) {
+        let red = redovi[i];
+        let celije = red.getElementsByTagName('td');
+        for (let j = 0; j < celije.length; j++) {
+            let celija = celije[j];
+            celija.style.backgroundColor = '';
+            celija.style.boxShadow = '';
+        }
+    }
 
+    for (let i = 0; i < K.length; i++) {
+        let coord = K[i];
+        if (coord.i < redovi.length) {
+            let red = redovi[coord.i];
+            let celije = red.getElementsByTagName('td');
+            if (coord.j < celije.length) {
+                let celija = celije[coord.j];
+                celija.style.backgroundColor = 'red';
+                celija.style.boxShadow = 'inset 0 0 20px white';
+            }
+        }
+    }
+}
+
+function spustiKoordinate() {
+    let mozeSeSpustiti = true;
+    for (let i = 0; i < K.length; i++) {
+        
+        K[i].i += 1;
+    }
+
+    let redovi = document.getElementById('tetris').getElementsByTagName('tr');
+    let maxI = Math.max(...K.map(coord => coord.i));
+    if (maxI >= redovi.length) {
+        K = [
+            { i: 0, j: 4 },
+            { i: 0, j: 5 },
+            { i: 1, j: 4 },
+            { i: 1, j: 5 }
+        ];
+    }
+    obojiKoordinate();
+}
+
+const interval = 500;
+setInterval(spustiKoordinate, interval);
+
+document.onkeydown = function(event) {
+    switch (event.keyCode) {
+        case 37: // Levo
+            let levoOk = true;
+            for (let i = 0; i < K.length; i++) {
+                if (K[i].j <= 0) {
+                    levoOk = false;
+                    break;
+                }
+            }
+
+            if (levoOk) {
+                for (let i = 0; i < K.length; i++) {
+                    K[i].j -= 1;
+                }
+                obojiKoordinate();
+            }
+            break;
+
+        case 39: // Desno
+            let desnoOk = true;
+            for (let i = 0; i < K.length; i++) {
+                if (K[i].j >= 9) {
+                    desnoOk = false;
+                    break;
+                }
+            }
+
+            if (desnoOk) {
+                for (let i = 0; i < K.length; i++) {
+                    K[i].j += 1;
+                }
+                obojiKoordinate();
+            }
+            break;
+    }
+};
 
 
