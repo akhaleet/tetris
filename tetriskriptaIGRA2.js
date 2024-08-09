f77c8fe64bf656532dc27f53c274cf1665032ab4
akhaleet <bojantomovic333@gmail.com>
2024-08-09 06:30:58 +0200

    Fixed some bugs, my game now saves all the shapes when they fall all the way down or if they collide, and after that it instantly drops a new shape at the top.

--- tetriskriptaIGRA2.js
+++ tetriskriptaIGRA2.js
@@ -1,21 +1,21 @@
+let K = [
+    { i: 0, j: 4 },
+    { i: 0, j: 5 },
+    { i: 1, j: 4 },
+    { i: 1, j: 5 }
+];
 
-let K = [];
-K[1] = { i: 0, j: 4 };
-K[2] = { i: 0, j: 5 };
-K[3] = { i: 1, j: 4 };
-K[4] = { i: 1, j: 5 };
+let oldKocke = [];
 
-let novaKocka = false;
-
-function novaKockaDrop(){
+function novaKockaDrop() {
     K = [
         { i: 0, j: 4 },
         { i: 0, j: 5 },
         { i: 1, j: 4 },
         { i: 1, j: 5 }
     ];
-
 }
+
 
 function obojiKoordinate(){
     let tabela = document.querySelector('#tetris table');
@@ -25,16 +25,29 @@ function obojiKoordinate(){
     for (let i=0; i<redovi.length; i++){
         let red = redovi[i];
         let celija = red.getElementsByTagName('td');
-        for (j=0; j<celija.length; j++){
+        for (let j=0; j<celija.length; j++){
             let celije = celija[j];
             celije.style.backgroundColor = '';
             celije.style.boxShadow = '';
         }
     } // cisti cijelu tabelu
 
+    for (let k = 0; k < oldKocke.length; k++) {
+        let koordinate = oldKocke[k];
+        if (koordinate.i < redovi.length) {
+            let red = redovi[koordinate.i];
+            let celije = red.getElementsByTagName('td');
+            if (koordinate.j < celije.length) {
+                let celija = celije[koordinate.j];
+                celija.style.backgroundColor = 'red';
+                celija.style.boxShadow = 'inset 0 0 20px white';
+            }
+        }
+    } // obojaj staru kocku
 
 
-    for (let k = 1; k < K.length; k++) {
+
+    for (let k = 0; k < K.length; k++) {
         let koordinate = K[k];
         if (koordinate.i < redovi.length) {
             let red = redovi[koordinate.i];
@@ -45,7 +58,23 @@ function obojiKoordinate(){
                 celija.style.boxShadow = 'inset 0 0 20px white';
             }
         }
-    } // obojaj kocku
+    } // obojaj trenutnu kocku
+
+}
+
+function provjeriKoliziju() {
+    let tabela = document.querySelector('#tetris table');
+    let redovi = tabela.rows;
+    
+    for (i=0; i<K.length; i++){
+        let kocka = K[i];
+
+        if (oldKocke.some(oldKocke => oldKocke.i-1 === kocka.i && oldKocke.j === kocka.j)){
+            return true;
+        }
+    }
+    return false;
+    
 }
 
 document.onkeydown = function(event) {
@@ -63,7 +92,7 @@ document.onkeydown = function(event) {
     switch (event.keyCode) {
         case 37: // Levo
             let levoOk = true;
-            for (let i = 1; i < K.length; i++) {
+            for (let i = 0; i < K.length; i++) {
                 if (K[i].j <= 0) {
                     levoOk = false;
                     break;
@@ -71,7 +100,7 @@ document.onkeydown = function(event) {
             }
 
             if (levoOk) {
-                for (let i = 1; i < K.length; i++) {
+                for (let i = 0; i < K.length; i++) {
                     K[i].j -= 1;
                 }
                 obojiKoordinate();
@@ -80,7 +109,7 @@ document.onkeydown = function(event) {
 
         case 39: // Desno
             let desnoOk = true;
-            for (let i = 1; i < K.length; i++) {
+            for (let i = 0; i < K.length; i++) {
                 if (K[i].j >= 9) {
                     desnoOk = false;
                     break;
@@ -88,10 +117,11 @@ document.onkeydown = function(event) {
             }
 
             if (desnoOk) {
-                for (let i = 1; i < K.length; i++) {
+                for (let i = 0; i < K.length; i++) {
                     K[i].j += 1;
                 }
                 obojiKoordinate();
+
             }
             break;
     }
@@ -102,26 +132,29 @@ setInterval(function spustajElement() {
     let redovi = tabela.rows;
     provjera = true;
 
-    for (let i = 1; i < K.length; i++) {
+    for (let i = 0; i < K.length; i++) {
         if (K[i].i >= redovi.length-1) {
                 provjera = false;
                 break;
             }
         }
+        if (provjeriKoliziju()) {
+            oldKocke = oldKocke.concat(K);
+            novaKockaDrop();
+        }
+
             if (!provjera) {
-                obojiKoordinate();
+                oldKocke = oldKocke.concat(K);
+                novaKockaDrop();
             }
             else {
-                for (let i = 1; i < K.length; i++) {
+                for (let i = 0; i < K.length; i++) {
                     K[i].i += 1;
             }
-            obojiKoordinate();
+            
         }
+        obojiKoordinate();
 }, 500);
-
-
-
-
 
 
 obojiKoordinate();
